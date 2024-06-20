c9e3916c9313f74952db1e97262becdc9d1aae2a
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-20 09:52:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 
-class A {
-pudlic:
-static A* getInstance() {std::cout<< "get A" << std::endl;
-if (instance) return instance;
-return new A;
-}
-private:
-A() {std::cout << "construct A<< std::endl;}
-A* instance;
-    };
 int main () {
   A* instance;
 };
