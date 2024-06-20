d944dc50cd4bfa063be3ebeab641f3d82910770b
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-20 10:32:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
 int main () {
-  A* instance;
-};
-int main () {
-  A* pa1 = A.getinstance();
-  A* pa2 = A.getinstance();
+    A* pa1 = A::getinstance();
+    A* pa2 = A::getinstance();
 }
