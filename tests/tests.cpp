ee009e257c0b6a6acdd0b9b51ceafba795fd579e
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-20 11:50:59 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,39 @@
-cmake_minimum_required(VERSION 3.10)
-project(z2)
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "A.h"
 
-add_executable(z2 main.cpp)
+class MockA {
+public:
+    MOCK_METHOD0(getInstance, A*());
+    MOCK_METHOD0(AConstructor, void());
+};
 
-add_subdirectory(tests)
+MockA* mockA = nullptr;
+
+A* A::getInstance() {
+    static A instance;  
+    return &instance;
+    return mockA->getInstance();
+}
+
+A::A() {
+      std::cout << "construct " << std::endl;
+      mockA->AConstructor();
+}
+
+TEST(ATest, GetInstanceCalledOnce) {
+    MockA mock;
+    mockA = &mock;
+
+    EXPECT_CALL(mock, AConstructor()).Times(1);
+    
+    A* pa1 = A::getInstance();
+    A* pa2 = A::getInstance();
+
+    mockA = nullptr;
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
