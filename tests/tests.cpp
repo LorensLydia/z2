00d5572f72478c2f8045a317935c2f806a93b1b1
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-20 10:06:42 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,32 +1,6 @@
-#include <iostream>
-#include <gtest/gtest.h>
+cmake_minimum_required(VERSION 3.10)
+project(z2)
 
-class A {
-public:
-    static A* getInstance() {
-        std::cout << "get A" << std::endl;
-        if (instance) return instance;
-        return new A;
-    }
+add_executable(z2 main.cpp)
 
-private:
-    A() {
-        std::cout << "construct A" << std::endl;
-    }
-
-    static A* instance;
-};
-
-A* A::instance = nullptr;
-
-TEST(AConstructorTest, ConstructorCalledOnce) {
-    A* pa1 = A::getInstance();
-    A* pa2 = A::getInstance();
-
-    EXPECT_EQ(pa1, pa2); // Убедимся, что указатели на объекты совпадают
-}
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+add_subdirectory(tests)
