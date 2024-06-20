e5191976d904381a0f59563e180bc593e2ffa803
LorensLydia <155399507+LorensLydia@users.noreply.github.com>
2024-06-20 09:53:05 +0300

    Update class.hpp

--- class.hpp
+++ class.hpp
@@ -1,1 +1,10 @@
-
+class A {
+pudlic:
+static A* getInstance() {std::cout<< "get A" << std::endl;
+if (instance) return instance;
+return new A;
+}
+private:
+A() {std::cout << "construct A<< std::endl;}
+A* instance;
+    };
